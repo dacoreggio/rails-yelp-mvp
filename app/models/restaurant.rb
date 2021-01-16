eb33e1ab08f03cb789015f6ec82810ad64403f57
Leonardo Dacoreggio <dacoreggio@gmail.com>
2021-01-16 11:24:20 +0100

    clean status

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :reviews
 
   validates :name, presence: true
-  validates :category, presence: true
-  validates :category, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, presence: true, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"] }
   validates :address, presence: true
+  validates :phone_number, presence: true
 end
